4cd3c9b86d5c3474358cd5fce066a8d9ca192aa4
Camden Reslink <camdenreslink@gmail.com>
2018-02-25 01:38:43 -0500

    Set up initial fsharp project with Paket.

---
+++ Mono.Cecil_FunTricks/Mono.Cecil_FunTricks/Program.fs
@@ -0,0 +1,10 @@
+﻿open Mono.Cecil
+
+let openFile (filePath:string) =
+    let assembly = AssemblyDefinition.ReadAssembly filePath
+    assembly
+
+[<EntryPoint>]
+let main argv = 
+    let assembly = openFile argv.[0]
+    0 // return an integer exit code
